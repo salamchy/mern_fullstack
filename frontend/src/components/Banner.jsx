1a04a9525b0091bf97c20be181b1c512e0b9401d
salam <chaudharysm00@gmail.com>
2025-01-01 23:49:17 +0545

    banner

--- frontend/src/components/Banner.jsx
+++ frontend/src/components/Banner.jsx
@@ -1,6 +1,21 @@
+import { Link } from "react-router-dom";
+import BannerImg from "../assets/header.png";
+
 const Banner = () => {
   return (
-    <div>Banner</div>
+    <div className="section__container header__container">
+      <div className="header__content z-30">
+        <h4 className="uppercase">UP TO 20% DISCOUNT ON </h4>
+        <h1>Girl's Fashion</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est repudiandae, similique inventore perspiciatis harum accusantium corrupti veniam ex autem quae blanditiis libero at ut?</p>
+        <button className="btn"><Link to="/shop">EXPLORE NOW</Link></button>
+      </div>
+
+      <div className="header__image">
+        <img src={BannerImg} alt="banner img" />
+      </div>
+
+    </div>
   )
 }
 export default Banner
